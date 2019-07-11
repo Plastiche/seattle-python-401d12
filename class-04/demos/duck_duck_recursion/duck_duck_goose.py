f8bb7266a1eecadb721d40dbcb01ee3da0f25828
JB <jb.tellez@gmail.com>
2019-07-11 11:11:48 -0700

    moves studs

--- class-04/demos/duck_duck_recursion/duck_duck_goose.py
+++ class-04/demos/duck_duck_recursion/duck_duck_goose.py
@@ -1,7 +1,51 @@
 from os import listdir
 from os.path import isdir, abspath
 
+continue_searching = True
+
 def goose_check(folder):
+
+    global continue_searching
+
+    if not continue_searching:
+        return
+          
+    try:
+        with open(folder + '/duck.txt') as f:
+            contents = f.read()
+            print('duck contents', contents)
+            if contents == 'goose':
+                print(folder)
+                continue_searching = False
+
+    except FileNotFoundError:
+  
+        folder_path = abspath(folder)
+        file_names = listdir(folder_path)
+       
+        for file_name in file_names:
+            file_path = f'{folder_path}/{file_name}'
+            if isdir(file_path):
+                goose_check(file_path)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def goose_check_old(folder):
     folder_path = abspath(folder)
     file_names = listdir(folder_path)
     for file_name in file_names:
@@ -17,11 +61,8 @@ def goose_check(folder):
                     print(file_path)
                     print('*******************')
                 
-                
             
 
-
-
 if __name__ == "__main__":
-    goose_check('.')
+    goose_check('studs')
 
---
+++ class-04/demos/duck_duck_recursion/scratch.py
@@ -0,0 +1,8 @@
+matrix = [
+    ['a','b','c'],
+    ['d','e','f'],
+    ['g','h','i'],
+]
+
+for row in matrix:
+    print(''.join(row))
